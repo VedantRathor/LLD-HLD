58a0b06c529663a7014d75cf0bd36fb7b2d65b1c
VedantRathor <92686119+VedantRathor@users.noreply.github.com>
2025-11-24 04:57:01 +0530

    refactoring

--- src/HLD/HLD/Problems/Bit/ly/BitLy.java
+++
@@ -1,7 +0,0 @@
-package HLD.Problems.Bit.ly;
-
-public class BitLy {
-    public static void main(String[] varargs) {
-        System.out.println("Bit ly is running fine....");
-    }
-}
--- src/HLD/HLD/Problems/WhatsApp/WhatsApp.java
+++
@@ -1,5 +0,0 @@
-public class WhatsApp {
-    public static void main(String varargs[]) {
-        System.out.println("whatsapp is running fine...");
-    }
-}
