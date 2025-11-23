09e04f53fee8a976433b51682202a04a657a15c9
VedantRathor <92686119+VedantRathor@users.noreply.github.com>
2025-11-24 05:04:08 +0530

    Advance LLD

---
+++ src/LLD/AdvanceLLD/SnakesAndLadders/SnakesAndLadders.java
@@ -0,0 +1,7 @@
+package LLD.AdvanceLLD.SnakesAndLadders;
+
+public class SnakesAndLadders {
+    public static void main(String[] args) {
+        System.out.println("Snakes and Ladders..");
+    }
+}
---
+++ src/LLD/AdvanceLLD/TicTacToe/TicTacToe.java
@@ -0,0 +1,7 @@
+package LLD.AdvanceLLD.TicTacToe;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("TicTacToe..");
+    }
+}
